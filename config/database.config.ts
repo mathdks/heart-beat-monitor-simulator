889e5c2f4d040d830dd91d215c1a5c63ccb349d0
Matheus Rocha <matheus.srocha@yahoo.com.br>
2024-07-12 02:25:38 -0300

    fix: correção do nome da variável de ambiente

--- config/database.config.ts
+++ config/database.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE,
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10),
   username: process.env.DB_USERNAME,
