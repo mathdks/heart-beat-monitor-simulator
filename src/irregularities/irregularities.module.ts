11ee4ccef5f12dac91590a4f96883c87c920565f
Matheus Rocha <matheus.srocha@yahoo.com.br>
2024-07-15 19:42:18 -0300

    feat: atualização de providers e exports

--- src/irregularities/irregularities.module.ts
+++ src/irregularities/irregularities.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Irregularity } from './domain/entities/irregularity.entity';
 import { IrregularitiesController } from './infra/presentation/irregularities.controller';
+import { CreateIrregularityUseCase } from './application/use-cases/createIrregularity.use-case';
+import { IrregularityRepository } from './domain/repository/irregularity.repository';
+import { IrregularityAdapter } from './infra/adapters/irregularity.adapter';
+import { FinishIrregularityUseCase } from './application/use-cases/finishIrregularity.use-case';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Irregularity])],
   controllers: [IrregularitiesController],
-  providers: [],
+  providers: [
+    CreateIrregularityUseCase,
+    FinishIrregularityUseCase,
+    {
+      provide: IrregularityRepository,
+      useClass: IrregularityAdapter,
+    },
+  ],
+  exports: [CreateIrregularityUseCase, FinishIrregularityUseCase],
 })
 export class IrregularitiesModule {}
