a58630f0fb57c58d243cb265514ff2e210eeab3a
Matheus Rocha <matheus.srocha@yahoo.com.br>
2024-07-16 17:47:15 -0300

    feat: definição do usecase no modulo

--- src/measurements/measurements.module.ts
+++ src/measurements/measurements.module.ts
@@ -8,6 +8,7 @@ import { MeasurementRepository } from './domain/repository/measurements.reposito
 import { MeasurementAdapter } from './infra/adapters/measurement.adapter';
 import { IrregularitiesModule } from 'src/irregularities/irregularities.module';
 import { CountIrregularitiesOnLast60Measurements } from './application/use-cases/countIrregularitiesOnLast60Measurements.use-case';
+import { FindAllMeasurementsUseCase } from './application/use-cases/findAllMeasurements.use-case';
 @Module({
   imports: [TypeOrmModule.forFeature([Measurement]), IrregularitiesModule],
   controllers: [MeasurementsController],
@@ -15,6 +16,7 @@ import { CountIrregularitiesOnLast60Measurements } from './application/use-cases
     MeasurementsGateway,
     CreateMeasurementUseCase,
     CountIrregularitiesOnLast60Measurements,
+    FindAllMeasurementsUseCase,
     {
       provide: MeasurementRepository,
       useClass: MeasurementAdapter,
